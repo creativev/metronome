701e0be25270fb3a5bd0a8933b87f060e56656cb
Amir <amir.nagri+git@gmail.com>
2014-09-18 11:42:05 +0530

    controlling bpm via controls;

--- app/src/main/java/me/creativei/metronome/MainActivity.java
+++ app/src/main/java/me/creativei/metronome/MainActivity.java
@@ -8,13 +8,13 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.ToggleButton;
 
 import java.util.Timer;
 import java.util.TimerTask;
-
-import static me.creativei.metronome.Constants.LOG_TAG;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -23,6 +23,7 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         new BeatsWidget(this).start();
     }
 
@@ -48,8 +49,15 @@ public class MainActivity extends ActionBarActivity {
         private Timer timer;
         private ImageButton[] beatsButtons = new ImageButton[4];
         private int selected;
+        private TextView txtBpm;
+        private MainActivity context;
+        private final int beatSound;
+        private final Button btnUp;
+        private final Button btnDown;
 
         private BeatsWidget(final MainActivity context) {
+            this.context = context;
+
             beatsButtons[0] = (ImageButton) context.findViewById(R.id.imgBeat1);
             beatsButtons[1] = (ImageButton) context.findViewById(R.id.imgBeat2);
             beatsButtons[2] = (ImageButton) context.findViewById(R.id.imgBeat3);
@@ -57,63 +65,144 @@ public class MainActivity extends ActionBarActivity {
             selected = 0;
 
             soundPool = new SoundPool(5, AudioManager.STREAM_MUSIC, 0);
-            final int beatSound = soundPool.load(context, R.raw.click, 1);
+            beatSound = soundPool.load(context, R.raw.click, 1);
 
             startButton = (ToggleButton) context.findViewById(R.id.btnStart);
+            txtBpm = (TextView) context.findViewById(R.id.txtBPM);
+            btnUp = (Button) context.findViewById(R.id.btnBPMUp);
+            btnDown = (Button) context.findViewById(R.id.btnBPMDown);
+        }
+
+        public void start() {
+            final BeatsTimer beatsTimer = new BeatsTimer(bpmToDelay(parseBpm()), new Runnable() {
+                @Override
+                public void run() {
+                    context.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            soundPool.play(beatSound, 1, 1, 1, 0, 1);
+                            if (selected == -1) {
+                                selected = 0;
+                                beatsButtons[selected].setImageResource(R.drawable.ic_beats_on);
+                            } else {
+                                beatsButtons[selected].setImageResource(R.drawable.ic_beats_off);
+                                selected = (selected + 1) % 4;
+                                beatsButtons[selected].setImageResource(R.drawable.ic_beats_on);
+                            }
+                        }
+                    });
+                }
+            }, new Runnable() {
+                @Override
+                public void run() {
+                    context.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            beatsButtons[selected].setImageResource(R.drawable.ic_beats_off);
+                            selected = -1;
+                        }
+                    });
+                }
+            });
+
             startButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     if (startButton.isChecked()) {
-                        timer = new Timer(true);
-                        timer.scheduleAtFixedRate(new TimerTask() {
-                            @Override
-                            public void run() {
-                                context.runOnUiThread(new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        soundPool.play(beatSound, 1, 1, 1, 0, 1);
-                                        if (selected == -1) {
-                                            selected = 0;
-                                            beatsButtons[selected].setImageResource(R.drawable.ic_beats_on);
-                                        } else {
-                                            beatsButtons[selected].setImageResource(R.drawable.ic_beats_off);
-                                            selected = (selected + 1) % 4;
-                                            beatsButtons[selected].setImageResource(R.drawable.ic_beats_on);
-                                        }
-                                    }
-                                });
-                            }
-                        }, 0, 1000);
-
+                        beatsTimer.start();
                     } else {
-                        if (timer != null) {
-                            timer.cancel();
-                            timer = null;
-                        }
-                        context.runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                beatsButtons[selected].setImageResource(R.drawable.ic_beats_off);
-                                selected = -1;
-                            }
-                        });
+                        beatsTimer.stop();
                     }
                 }
             });
+            btnUp.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    int initialBpm = parseBpm();
+                    int finalBpm = initialBpm - (initialBpm % 10) + 10;
+                    txtBpm.setText(finalBpm + " BPM");
+                    beatsTimer.update(bpmToDelay(finalBpm));
+                    return true;
+                }
+            });
+            btnUp.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int initialBpm = parseBpm();
+                    int finalBpm = initialBpm + 1;
+                    txtBpm.setText(finalBpm + " BPM");
+                    beatsTimer.update(bpmToDelay(finalBpm));
+                }
+            });
 
-//            Button resetButton = (Button) context.findViewById(R.id.btnReset);
-//            resetButton.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View v) {
-//                    beatsButtons[selected].setBackgroundResource(R.drawable.ic_beats_off);
-//                    selected = 0;
-//                    beatsButtons[selected].setBackgroundResource(R.drawable.ic_beats_on);
-//                }
-//            });
+            btnDown.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    int initialBpm = parseBpm();
+                    int sub = initialBpm % 10 == 0 ? 10 : initialBpm % 10;
+                    int finalBpm = initialBpm - sub;
+                    txtBpm.setText(finalBpm + " BPM");
+                    beatsTimer.update(bpmToDelay(finalBpm));
+                    return true;
+                }
+            });
+            btnDown.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int initialBpm = parseBpm();
+                    int finalBpm = initialBpm - 1;
+                    txtBpm.setText(finalBpm + " BPM");
+                    beatsTimer.update(bpmToDelay(finalBpm));
+                }
+            });
+        }
+
+        private int bpmToDelay(int finalBpm) {
+            return (int) (60.0 * 1000.0 / finalBpm);
+        }
+
+        private int parseBpm() {
+            return Integer.parseInt(txtBpm.getText().toString().split(" ")[0]);
+        }
+    }
+
+    public static class BeatsTimer {
+        private int delay;
+        private Runnable onStartTask;
+        private Runnable onStopTask;
+        private Timer timer;
+        private boolean isRunning;
+
+        public BeatsTimer(int delay, Runnable onStartTask, Runnable onStopTask) {
+            this.delay = delay;
+            this.onStartTask = onStartTask;
+            this.onStopTask = onStopTask;
         }
 
         public void start() {
+            isRunning = true;
+            timer = new Timer(true);
+            timer.scheduleAtFixedRate(new TimerTask() {
+                @Override
+                public void run() {
+                    onStartTask.run();
+                }
+            }, 0, delay);
+        }
 
+        public void update(int delay) {
+            this.delay = delay;
+            Log.d(Constants.LOG_TAG, "Delay is " + delay);
+            if (isRunning) {
+                stop();
+                start();
+            }
+        }
+
+        public void stop() {
+            timer.cancel();
+            isRunning = false;
+            onStopTask.run();
         }
     }
 }
