a81351bfb851eddd38acc9825e272184a6bd5c59
Amir <amir.nagri+git@gmail.com>
2014-09-23 08:19:29 +0530

    integrating analytics

--- app/src/main/java/me/creativei/metronome/MainActivity.java
+++ app/src/main/java/me/creativei/metronome/MainActivity.java
@@ -3,6 +3,7 @@ package me.creativei.metronome;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -12,12 +13,16 @@ import android.widget.ToggleButton;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.analytics.GoogleAnalytics;
+import com.google.android.gms.analytics.HitBuilders;
+import com.google.android.gms.analytics.Tracker;
 
 
 public class MainActivity extends ActionBarActivity implements BeatFragment.Callback {
 
     public static final String PREF_KEEP_SCREEN_ON = "KEEP_SCREEN_ON";
+    public static final String TEST_DEVICE = "D27BE559F36AC73AFA3ED3E64322B072";
     private BeatsWidget beatsWidget;
+    private Tracker tracker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,18 +36,26 @@ public class MainActivity extends ActionBarActivity implements BeatFragment.Call
 
         AdRequest adRequest = new AdRequest.Builder()
                 .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .addTestDevice(TEST_DEVICE)
                 .build();
         AdView adView = (AdView) findViewById(R.id.adView);
         adView.loadAd(adRequest);
 
         GoogleAnalytics analytics = GoogleAnalytics.getInstance(this);
-        analytics.newTracker(R.xml.tracker);
+        tracker = analytics.newTracker(R.xml.tracker);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         beatsWidget.onResume();
+        tracker.setScreenName("me.creativei.metronome.MainActivity");
+        tracker.send(new HitBuilders.AppViewBuilder().build());
     }
 
     @Override
@@ -55,29 +68,6 @@ public class MainActivity extends ActionBarActivity implements BeatFragment.Call
     protected void onStop() {
         super.onStop();
         this.onPause();
-    }
-
-    private void setupScreenOnButton() {
-        final ToggleButton screenOn = (ToggleButton) findViewById(R.id.btnScreenOn);
-        boolean keepScreenWake = getAppStatePref().getBoolean(PREF_KEEP_SCREEN_ON, false);
-        screenOn.setChecked(keepScreenWake);
-        screenOn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                SharedPreferences preferences = getAppStatePref();
-                SharedPreferences.Editor editor = preferences.edit();
-                ToggleButton screenOnBtn = (ToggleButton) v;
-                editor.putBoolean(PREF_KEEP_SCREEN_ON, screenOnBtn.isChecked());
-                editor.apply();
-
-                if (screenOnBtn.isChecked())
-                    getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-                else
-                    getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-            }
-        });
-        if (keepScreenWake)
-            getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
     }
 
     private SharedPreferences getAppStatePref() {
@@ -102,5 +92,28 @@ public class MainActivity extends ActionBarActivity implements BeatFragment.Call
     @Override
     public void beatPlayed() {
         beatsWidget.beatPlayed();
+    }
+
+    private void setupScreenOnButton() {
+        final ToggleButton screenOn = (ToggleButton) findViewById(R.id.btnScreenOn);
+        boolean keepScreenWake = getAppStatePref().getBoolean(PREF_KEEP_SCREEN_ON, false);
+        screenOn.setChecked(keepScreenWake);
+        screenOn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences preferences = getAppStatePref();
+                SharedPreferences.Editor editor = preferences.edit();
+                ToggleButton screenOnBtn = (ToggleButton) v;
+                editor.putBoolean(PREF_KEEP_SCREEN_ON, screenOnBtn.isChecked());
+                editor.apply();
+
+                if (screenOnBtn.isChecked())
+                    getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+                else
+                    getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+            }
+        });
+        if (keepScreenWake)
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
     }
 }
